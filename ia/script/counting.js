4c7e725354fb28d3c86f2acb4b3b26c3fa330548
wanoneyor <100901670+wanoneyor@users.noreply.github.com>
2022-08-04 10:34:05 +0800

    Add files via upload

---
+++ ia/script/counting.js
@@ -0,0 +1,15 @@
+const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        target.innerText = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+};
+
+counterAnim("#counter", 0, 700/*mark*/, 4000);
