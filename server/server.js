93c954a56bc6ce153ea422bc5f0ccb5d9f0f158a
unknown <a1@tyit35648.vtc.hk>
2022-08-15 13:45:21 +0800

    Add scoreboard database and get / post handling

--- server/server.js
+++ server/server.js
@@ -19,10 +19,41 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
 const fs = require("fs");
 const util_1 = require("util");
+const lmdb_1 = require("lmdb"); // or require
+function isScoreboardEntry(arg) {
+    return arg
+        && arg.score && typeof (arg.score) == 'number'
+        && arg.duration && typeof (arg.duration) == 'number'
+        && arg.name && typeof (arg.name) == 'string'
+        && arg.level && typeof (arg.level) == 'number';
+}
 const port = process.env.port || 80;
 const baseWebDir = "../ia";
+const databaseDir = "../database/scoreboard";
+const requestScoreboardPath = "/scoreboard.json";
 const blacklist = [];
-var times = [];
+const MAX_NAME_LENGTH = 128;
+const SCOREBOARD_SIZE = 20;
+let database = (0, lmdb_1.open)({
+    path: databaseDir,
+    // any options go here, we can turn on compression like this:
+    compression: true,
+});
+let dbPosition = database.openDB({
+    name: "position"
+});
+let dbPersionalBest = database.openDB({
+    name: "personalBest"
+});
+// DB structure:
+//
+// [Score, (-)duration] -> {name, entry}
+// name -> Score, (-)duration // (The best one)
+function pairCompare(pairA, pairB) {
+    if (pairA[0] != pairB[0])
+        return pairA[0] - pairB[0];
+    return pairA[1] = pairB[1];
+}
 var server = http.createServer((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     Promise.race([
         (() => __awaiter(void 0, void 0, void 0, function* () {
@@ -66,6 +97,20 @@ function webGet(req, res) {
             res.writeHead(404);
             res.end();
             return true;
+        }
+        if (loc == requestScoreboardPath || '/' + loc == requestScoreboardPath) {
+            console.log("Scoreboard request.");
+            var topN = yield database.transaction(() => {
+                var topNArray = [];
+                dbPosition.getRange({ limit: SCOREBOARD_SIZE, reverse: true })
+                    .forEach(({ key, value }) => {
+                    topNArray.push({ score: key[0], duration: -key[1], name: value.name, level: value.level });
+                });
+                return topNArray;
+            });
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(JSON.stringify({ scoreboard: topN }));
+            return;
         }
         let fileDir = baseWebDir + loc;
         if (yield (0, util_1.promisify)(fs.exists)(fileDir)) {
@@ -135,12 +180,44 @@ function timePost(req, res) {
             var jData = JSON.parse(data);
             if (jData["postType"] != "time")
                 return false;
-            if (!(jData["duration"] instanceof Number))
+            if (!isScoreboardEntry(jData["entry"]))
                 return false;
-            var duration = jData["duration"];
-            times.push(duration);
+            let entry = jData["entry"];
+            // Verify the name string
+            if (entry.name.length > MAX_NAME_LENGTH) {
+                console.log("Error 400: entry name too long.");
+                res.writeHead(400);
+                res.end();
+                return true;
+            }
+            if (entry.level < 0 || entry.score < 0 || entry.duration <= 0) {
+                console.log("Error 400: Numbers outside valid range.");
+                res.writeHead(400);
+                res.end();
+                return true;
+            }
+            var topN = yield database.transaction(() => {
+                var scoreDurationPair = dbPersionalBest.get(entry.name);
+                if (scoreDurationPair === undefined) {
+                    dbPersionalBest.put(entry.name, [entry.score, -entry.duration]);
+                    dbPosition.put([entry.score, -entry.duration], { name: entry.name, level: entry.level });
+                }
+                else if (pairCompare(scoreDurationPair, [entry.score, -entry.duration]) > 0) {
+                    let worked = dbPosition.removeSync(scoreDurationPair);
+                    if (!worked)
+                        console.warn("Failed to remove old score from the position database! Possible database corruption!");
+                    dbPersionalBest.put(entry.name, [entry.score, -entry.duration]);
+                    dbPosition.put([entry.score, -entry.duration], { name: entry.name, level: entry.level });
+                }
+                var topNArray = [];
+                dbPosition.getRange({ limit: SCOREBOARD_SIZE, reverse: true })
+                    .forEach(({ key, value }) => {
+                    topNArray.push({ score: key[0], duration: -key[1], name: value.name, level: value.level });
+                });
+                return topNArray;
+            });
             res.writeHead(200, { "content-type": "application/json" });
-            res.end(JSON.stringify({ time: times }));
+            res.end(JSON.stringify({ scoreboard: topN }));
             return true;
         }
         catch (s) {
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,57 @@
 import http = require('http');
 import fs = require('fs');
 import { promisify } from 'util';
+import { open } from 'lmdb'; // or require
+import lmdb = require('lmdb');
+
+interface ScoreboardEntry {
+    score: number; // primary order
+    duration: number; // secondary order
+    name: string; // String is encoded to be in base64 format
+    level: number;
+}
+function isScoreboardEntry(arg: any): arg is ScoreboardEntry {
+    return arg
+        && arg.score && typeof (arg.score) == 'number'
+        && arg.duration && typeof (arg.duration) == 'number'
+        && arg.name && typeof (arg.name) == 'string'
+        && arg.level && typeof (arg.level) == 'number';
+}
+interface DBPosEntry {
+    name: string;
+    level: number;
+}
+
 const port = process.env.port || 80
 const baseWebDir = "../ia";
+const databaseDir = "../database/scoreboard"
+const requestScoreboardPath = "/scoreboard.json"
 const blacklist = [];
+const MAX_NAME_LENGTH = 128;
+const SCOREBOARD_SIZE = 20;
+let database = open({
+    path: databaseDir,
+    // any options go here, we can turn on compression like this:
+    compression: true,
+});
+let dbPosition: lmdb.Database<DBPosEntry, number[]> = database.openDB({
+    name: "position"
+});
+let dbPersionalBest: lmdb.Database<number[], string> = database.openDB({
+    name: "personalBest"
+});
 
-var times : Number[] = [];
+// DB structure:
+//
+// [Score, (-)duration] -> {name, entry}
+// name -> Score, (-)duration // (The best one)
+
+function pairCompare(pairA, pairB) : number {
+    if (pairA[0] != pairB[0]) return pairA[0] - pairB[0];
+    return pairA[1] = pairB[1];
+}
+
+
 
 var server = http.createServer(
     async (req, res) => {
@@ -34,7 +80,7 @@ server.listen(port);
 
 
 
-async function webGet(req: http.IncomingMessage, res: http.ServerResponse) : Promise<boolean> {
+async function webGet(req: http.IncomingMessage, res: http.ServerResponse): Promise<boolean> {
     let loc: String = req.url;
     if (loc == null) {
         console.log("bad request: No 'url'");
@@ -53,6 +99,22 @@ async function webGet(req: http.IncomingMessage, res: http.ServerResponse) : Pro
         res.end();
         return true;
     }
+
+    if (loc == requestScoreboardPath || '/' + loc == requestScoreboardPath) {
+        console.log("Scoreboard request.");
+        var topN = await database.transaction(() => {
+            var topNArray: ScoreboardEntry[] = [];
+            dbPosition.getRange({ limit: SCOREBOARD_SIZE, reverse: true })
+                .forEach(({ key, value }) => {
+                    topNArray.push({ score: key[0], duration: -key[1], name: value.name, level: value.level });
+                });
+            return topNArray;
+        });
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ scoreboard: topN }));
+        return;
+    }
+
 
     let fileDir = baseWebDir + loc;
     if (await promisify(fs.exists)(fileDir)) {
@@ -105,19 +167,45 @@ async function timePost(req: http.IncomingMessage, res: http.ServerResponse) {
     try {
         var jData = JSON.parse(data);
         if (jData["postType"] != "time") return false;
-        if (!(jData["duration"] instanceof Number)) return false; 
-        var duration: Number = jData["duration"];
-        times.push(duration);
+        if (!isScoreboardEntry(jData["entry"])) return false;
+
+        let entry : ScoreboardEntry = jData["entry"];
+        // Verify the name string
+        if (entry.name.length > MAX_NAME_LENGTH) {
+            console.log("Error 400: entry name too long.");
+            res.writeHead(400);
+            res.end();
+            return true;
+        }
+        if (entry.level < 0 || entry.score < 0 || entry.duration <= 0) {
+            console.log("Error 400: Numbers outside valid range.");
+            res.writeHead(400);
+            res.end();
+            return true;
+        } 
+        var topN = await database.transaction(() => {
+            var scoreDurationPair = dbPersionalBest.get(entry.name);
+            if (scoreDurationPair === undefined) {
+                dbPersionalBest.put(entry.name, [entry.score, -entry.duration]);
+                dbPosition.put([entry.score, -entry.duration], { name: entry.name, level: entry.level });
+            } else if (pairCompare(scoreDurationPair, [entry.score, -entry.duration]) > 0) {
+                let worked = dbPosition.removeSync(scoreDurationPair);
+                if (!worked) console.warn("Failed to remove old score from the position database! Possible database corruption!");
+                dbPersionalBest.put(entry.name, [entry.score, -entry.duration]);
+                dbPosition.put([entry.score, -entry.duration], { name: entry.name, level: entry.level });
+            }
+            var topNArray: ScoreboardEntry[] = [];
+            dbPosition.getRange({ limit: SCOREBOARD_SIZE, reverse: true })
+                .forEach(({ key, value }) => {
+                    topNArray.push({ score: key[0], duration: -key[1], name: value.name, level: value.level });
+                });
+            return topNArray;
+        });
         res.writeHead(200, { "content-type": "application/json" });
-        res.end(JSON.stringify({time: times}));
+        res.end(JSON.stringify({scoreboard: topN}));
         return true;
     } catch (s) {
         if ((s instanceof SyntaxError) || (s instanceof ReferenceError)) return false;
         throw s;
     }
-
-    
-
-
-
 }
