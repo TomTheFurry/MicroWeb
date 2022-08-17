b0747832aea765e9dee1aa5db8519d6e66fcc15c
unknown <a1@tyit35655.vtc.hk>
2022-08-17 12:15:53 +0800

    dark-mode

--- ia/script/app.js
+++ ia/script/app.js
@@ -20,7 +20,11 @@ const startPage = (idx) => {
     }
 }
 
-function darkMode() {
+const isUserIsDarkMode = () => { return window.matchMedia("(prefers-color-scheme: dark)").matches; }
+
+const isDarkMode = () => { return document.documentElement.classList.contains('dark-mode'); }
+
+const darkMode = () => {
     let rootElement = document.documentElement;
     rootElement.classList.toggle('dark-mode');
     if (rootElement.classList.contains('dark-mode')) {
@@ -34,8 +38,6 @@ const readJson = async (path) => {
     if (response.status === 200) { return await response.json(); }
     else { return null; }
 }
-
-const isUserIsDarkMode = () => { return window.matchMedia("(prefers-color-scheme: dark)").matches; }
 
 const GAME_BOX = document.getElementById('game-box');
 const END_BOX = document.getElementById('end-box');
@@ -66,8 +68,6 @@ var colorfulTextParent = [];
 // }
 
 const startGamePage = async () => {
-    if (isUserIsDarkMode()) { darkMode(); }
-
     showPage(GAME_BOX);
 
     let intro = GAME_BOX.querySelector('.intro');
@@ -156,14 +156,22 @@ const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
 // };
 
 const assignColorfulText = async () => {
-    let regex = /\s/;
+    const defaultColor = '#000'
+    const regex = /\s/;
+    let colorTexts = document.getElementsByClassName('colorful-texts');
+    if (colorTexts.length === 0) { return false; }
 
     // colourfulText 'data/colourful-text.json'
     await readJson('data/colourful-text.json').then((colourfulText) => {
-        let colorTexts = document.getElementsByClassName('colorful-texts');
-
         for (let i = 0; i < colorTexts.length && colourfulText !== null; ++i) {
             let e = colorTexts.item(i);
+            // remove all child with class line
+            {
+                let children = e.getElementsByClassName('line');
+                for (let i = 0; i < children.length; ++i)
+                    e.removeChild(children.item(i));
+            }
+            
             let id = e.getAttribute('colorful-id');
             e.removeAttribute('colorful-id');
 
@@ -171,11 +179,9 @@ const assignColorfulText = async () => {
             e['id'] = id;
 
             let textData = colourfulText.find(e => e.id === id);
-            if (!colourfulText || typeof textData === "undefined") {
-                continue;
-            }
+            if (!colourfulText || typeof textData === "undefined") { continue; }
 
-            let colors = textData.colors;
+            let colors = isDarkMode() ? textData.darkColors : textData.colors;
             let colorIdx = 0;
             textData.texts.forEach((text, idx) => {
                 if (idx !== 0) {
@@ -192,8 +198,8 @@ const assignColorfulText = async () => {
                     
                     if (!regex.test(c)) {
                         newText.innerHTML = c;
-                        let color = '#000';
-                        if (colors.length >= 0) {
+                        let color = defaultColor;
+                        if (colors.length > 0) {
                             color = '';
                             if (colors[colorIdx].charAt(0) !== '#') { color += '#'; }
                             color += colors[colorIdx++];
@@ -216,14 +222,50 @@ const assignColorfulText = async () => {
             });
         }
     });
+
+    return true;
 }
 
-const updateColorfulText = () => {
-    
+const updateColorfulText = async () => {
+    const defaultColor = '#000';
+    const regex = /\s/;
+    if (colorfulTextParent.length === 0 && !assignColorfulText()) { return false; }
+
+    await readJson('data/colourful-text.json').then((colourfulText) => {
+        colorfulTextParent.forEach((e) => {
+            let id = e['id'];
+            let textData = colourfulText.find(e => e.id === id);
+
+            let colors = isDarkMode() ? textData.darkColors : textData.colors;
+            let colorIdx = 0;
+
+            let lines = e.getElementsByClassName('line');
+            for (let i = 0; i < lines.length; ++i) {
+                let line = lines.item(i);
+                let chars = line.getElementsByClassName('colorful-text');
+
+                for (let i = 0; i < chars.length; ++i) {
+                    let char = chars.item(i);
+                    if (!regex.test(char)) {
+                        let color = defaultColor;
+                        if (colors.length > 0) {
+                            color = '';
+                            if (colors[colorIdx].charAt(0) !== '#') { color += '#'; }
+                            color += colors[colorIdx++];
+                        }
+                        newText.style.setProperty('--color', color);
+                    }
+                }
+            }
+        });
+    });
+
+    return true;
 }
 
 const initPage = () => {
     new Promise(async () => {
+        if (isUserIsDarkMode()) { darkMode(); }
         await assignColorfulText();
         includeHTML();
         startPage(1);
